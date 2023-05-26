56c7a753dba9231851569c501f9c629119fd9b75
Oleg <oleg2016te@gmail.com>
2023-05-26 23:13:38 +0300

    Initial commit

---
+++ thread/src/main/java/ru/mirea/feofanovoi/thread/MainActivity.java
@@ -0,0 +1,27 @@
+package ru.mirea.feofanovoi.thread;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView infoTextView = findViewById(R.id.threadTextMirea);
+        Thread mainThread = Thread.currentThread();
+        infoTextView.setText("Имя текущего потока: " + mainThread.getName());
+// Меняем имя и выводим в текстовом поле
+        mainThread.setName("МОЙ НОМЕР ГРУППЫ: XX, НОМЕР ПО СПИСКУ: XX, МОЙ ЛЮБИИМЫЙ ФИЛЬМ: XX");
+        infoTextView.append("\n Новое имя потока: " + mainThread.getName());
+        Log.d(MainActivity.class.getSimpleName(), "Stack: " + Arrays.toString(mainThread.getStackTrace()));
+    }
+
+}
